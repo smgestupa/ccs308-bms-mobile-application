c858f7ebd138ed6976782b30aa7a96d813632f9a
smgestupa <thereal.nwahs@gmail.com>
2023-06-24 12:42:31 +0800

    integrate daisyui to project

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,7 +6,24 @@ const config = {
 		extend: {}
 	},
 
-	plugins: []
+	plugins: [require("daisyui")],
+
+	daisyui: {
+		themes: [{
+			light: {
+				"primary": "#4be2ed",
+				"secondary": "#c3f45a",
+				"accent": "#f79eec",
+				"neutral": "#4f4455",
+				"base-100": "#32434e",
+				"info": "#6984ce",
+				"success": "#3de18c",
+				"warning": "#f3ab5e",
+				"error": "#f87272"
+			}
+		}],
+		darkTheme: 'false'
+	}
 };
 
 module.exports = config;
