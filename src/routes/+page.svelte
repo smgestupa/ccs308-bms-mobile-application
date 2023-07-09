5e2ff973fa768ca62e9b5d8a1fa5d1352d7d9ecf
smgestupa <thereal.nwahs@gmail.com>
2023-07-09 14:06:14 +0800

    redirect users to login page if no jwtToken exists, otherwise to the home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,14 @@
-<script lang="ts">
-    import { Geolocation } from "@capacitor/geolocation";
-	import type { Position } from "@capacitor/geolocation";
+<script>
+    import { goto } from "$app/navigation";
+    import { jwtToken } from "$lib/stores/jwt";
+	import { onMount } from "svelte";
 
-    let loc: Position | undefined = undefined;
-
-    const getCurrentLocation = async () => {
-        const res = await Geolocation.getCurrentPosition();
-        loc = res;
-    };
+    onMount(() => {
+        setTimeout(() => {
+            if ($jwtToken) goto("/home", { replaceState: true });
+            else goto("/login", { replaceState: true });
+        }, 1500);
+    });
 </script>
 
-<div>
-    <h1 class="font-bold text-3xl">Geolocation</h1>
-    <h2 class="text-xl">Your location is:</h2>
-    <p class="text-lg">Latitude: {loc?.coords.latitude}</p>
-    <p class="text-lg">Longitude: {loc?.coords.longitude}</p>
-
-    <button class="mt-4"
-    on:click={getCurrentLocation}>
-        Get Current Location
-    </button>
-</div>
+<p>RAAAAH</p>
