19ff14b89d02ad87c3affb994124536f8277f9bc
smgestupa <thereal.nwahs@gmail.com>
2023-07-09 23:30:58 +0800

    create component for the skeleton book display

---
+++ src/lib/components/SkeletonBookDisplay.svelte
@@ -0,0 +1,8 @@
+<div class="card card-compact w-fit h-fit p-1 rounded-none opacity-10">
+    <figure>
+        <div class="w-[132px] h-[211px] bg-white rounded-md bg-center bg-no-repeat bg-contain mx-auto bg-[url(/favicon.png)]"/>
+    </figure>
+    <p class="card-title inline-block mt-2 text-ellipsis overflow-hidden w-[132px] text-sm whitespace-nowrap">
+        <span class="block loading loading-spinner mx-auto"></span>
+    </p>
+</div>
