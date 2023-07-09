77418ecd28f83f99fab0cdc50045d7a8a3363b98
smgestupa <thereal.nwahs@gmail.com>
2023-07-09 23:09:28 +0800

    create component for display books

---
+++ src/lib/components/BookDisplay.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    export let bookID: number, cover: string, title: string;
+</script>
+
+<a class="card card-compact w-fit h-fit p-1 rounded-none"
+href="/book?id={bookID}">
+    <figure>
+        <img class="w-[132px] h-[211px] bg-white rounded-md bg-center bg-no-repeat bg-contain mx-auto" 
+        src="data:image/png;base64, {cover}" 
+        alt="">
+    </figure>
+    <p class="card-title inline-block mt-2 text-ellipsis overflow-hidden w-[132px] text-sm whitespace-nowrap">{title}</p>
+</a>
