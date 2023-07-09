d98bf87f8315682763b6cad1ed4ab1dc57412bc6
smgestupa <thereal.nwahs@gmail.com>
2023-07-09 22:48:01 +0800

    if both 'jwtToken' and 'userID' exists in the preferences, save to their respective stores

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,21 @@
 <script lang="ts">
     import { goto } from "$app/navigation";
     import { Preferences, type GetResult } from "@capacitor/preferences";
+    import { jwtToken, userID } from "$lib/stores/jwt";
 	import { onMount } from "svelte";
 
     onMount(() => {
         setTimeout(async () => {
-            const jwtToken: GetResult = await Preferences.get({ key: "jwtToken" });
+            const preferencesJwtToken: GetResult = await Preferences.get({ key: "jwtToken" });
+            const preferencesUserID: GetResult = await Preferences.get({ key: "userID" });
 
-            if (jwtToken.value) goto("/home", { replaceState: true });
-            else goto("/login", { replaceState: true });
+            if (preferencesJwtToken.value && preferencesUserID.value) {
+                $jwtToken = preferencesJwtToken.value;
+                $userID = Number(preferencesUserID.value);
+                goto("/home", { replaceState: true });
+            } 
+            else 
+                goto("/login", { replaceState: true });
         }, 1500);
     });
 </script>
