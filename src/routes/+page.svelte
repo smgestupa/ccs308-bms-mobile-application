90d75a7e1f7c0cbe8a536557b44d7bfee4c2ca29
smgestupa <thereal.nwahs@gmail.com>
2023-07-09 15:09:28 +0800

    use ts in <script>, use capacitor's preferences to check for the 'jwtToken' instead

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,13 @@
-<script>
+<script lang="ts">
     import { goto } from "$app/navigation";
-    import { jwtToken } from "$lib/stores/jwt";
+    import { Preferences, type GetResult } from "@capacitor/preferences";
 	import { onMount } from "svelte";
 
     onMount(() => {
-        setTimeout(() => {
-            if ($jwtToken) goto("/home", { replaceState: true });
+        setTimeout(async () => {
+            const jwtToken: GetResult = await Preferences.get({ key: "jwtToken" });
+
+            if (jwtToken.value) goto("/home", { replaceState: true });
             else goto("/login", { replaceState: true });
         }, 1500);
     });
